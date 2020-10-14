1b5ae95c3f4f537a02a2bd4541719a14144d2339
junyoong <huijy-wm19@student.tarc.edu.my>
2020-10-14 16:51:14 +0800

    Commit by hjy

--- app/src/main/java/com/example/adventure1/FirstScene.java
+++ app/src/main/java/com/example/adventure1/FirstScene.java
@@ -3,6 +3,8 @@ package com.example.adventure1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class FirstScene extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class FirstScene extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_scene);
-
+        public void onClick(View view) {
+            Intent intent = new Intent(FirstScene.this, Login.class);
+            startActivity(intent);
+        }
     }
 }
