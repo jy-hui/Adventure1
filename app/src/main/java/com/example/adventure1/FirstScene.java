db10ac61ed613409b3e8b3236cff7c75da52f8bc
junyoong <huijy-wm19@student.tarc.edu.my>
2020-10-14 16:29:07 +0800

    Commit by hjy

---
+++ app/src/main/java/com/example/adventure1/FirstScene.java
@@ -0,0 +1,15 @@
+package com.example.adventure1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstScene extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_scene);
+
+    }
+}
