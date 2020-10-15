df4e53809c66128a2584b62bac0c57fe859b5ade
junyoong <huijy-wm19@student.tarc.edu.my>
2020-10-15 16:30:49 +0800

    Commit By hjy

--- app/src/main/java/com/example/adventure1/FirstScene.java
+++ app/src/main/java/com/example/adventure1/FirstScene.java
@@ -2,19 +2,59 @@ package com.example.adventure1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FirstScene extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.first_scene);
-        public void onClick(View view) {
-            Intent intent = new Intent(FirstScene.this, Login.class);
-            startActivity(intent);
-        }
+
+        ///Variables
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.app_name);
+
+        logo.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent (FirstScene.this,Login.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(logo,"logo_image");
+                pairs[1] = new Pair<View,String>(title,"logo_text");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(FirstScene.this,pairs);
+                    startActivity(intent,options.toBundle());
+                    finish();
+                }
+            }
+        },SPLASH_SCREEN);
     }
 }
--- app/src/main/java/com/example/adventure1/Login.java
+++ app/src/main/java/com/example/adventure1/Login.java
@@ -2,13 +2,56 @@ package com.example.adventure1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.util.Pair;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
-
+    ImageButton goSignUp;
+    ImageView logo;
+    TextView title;
+    TextInputLayout email, password;
+    Button login_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.login);
+
+        goSignUp = findViewById(R.id.buttonSignUp);
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+        email = findViewById(R.id.titleEmail);
+        password = findViewById(R.id.titlePassword);
+        login_btn = findViewById(R.id.buttonLogin);
+
+        goSignUp.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                Pair[] pairs = new Pair[6];
+                pairs[0] = new Pair<View,String>(logo,"logo_image");
+                pairs[1] = new Pair<View,String>(title,"logo_text");
+                pairs[2] = new Pair<View,String>(email,"email_tran");
+                pairs[3] = new Pair<View,String>(logo,"password_tran");
+                pairs[4] = new Pair<View,String>(login_btn,"button_tran");
+                pairs[5] = new Pair<View,String>(goSignUp,"login_signup_tran");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this,pairs);
+                    startActivity(intent, options.toBundle());
+                }
+
+            }
+        });
+
     }
 }
